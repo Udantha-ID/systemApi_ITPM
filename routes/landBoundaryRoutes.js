6d5a6a4ce040a04775dccc1165d10a0b783cc68c
Induru Udantha <165542948+Udantha-ID@users.noreply.github.com>
2025-05-01 22:12:21 +0530

    Update LandBoundaryRoutes file

--- routes/landBoundaryRoutes.js
+++ routes/landBoundaryRoutes.js
@@ -1,1 +1,22 @@
-//Sample
+const express = require('express');
+const router = express.Router();
+
+// Import controller (to be created)
+const landBoundaryController = require('../controllers/landBoundaryController');
+
+// GET all land boundaries
+router.get('/', landBoundaryController.getAllLandBoundaries);
+
+// GET single land boundary by ID
+router.get('/:id', landBoundaryController.getLandBoundaryById);
+
+// POST create new land boundary
+router.post('/', landBoundaryController.createLandBoundary);
+
+// PUT update land boundary
+router.put('/:id', landBoundaryController.updateLandBoundary);
+
+// DELETE land boundary
+router.delete('/:id', landBoundaryController.deleteLandBoundary);
+
+module.exports = router;
